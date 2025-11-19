3fee17f336979fdb967af39d4def585aa1f44e8d
jonhp178 <phamtuananhpham46@gmail.com>
2025-11-19 08:44:57 +0700

    add file program

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace th_ham_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //bai3();
+            
+            
+
+        }
+        
+        static void nhap2so(string info, out int x, out int y) 
+        {
+            
+            Console.Write(info);
+            Console.Write("Nhap x: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Nhap y: ");
+            y = int.Parse(Console.ReadLine());
+        }
+        static void TinhToanNhieuPhep(int x, int y, out int tong, out int hieu, out int tich, out double thuong)
+        {
+            tong = x + y;   
+            hieu = x - y;
+            tich = x * y;
+            thuong = (double)x / y;
+
+        }
+        static void bai3()
+        {
+            int x, y;
+            nhap2so("bat dau nhap x y:", out x, out y);
+
+            int tong, hieu, tich;
+            double thuong;
+            TinhToanNhieuPhep(x, y, out tong, out hieu, out tich, out thuong);
+            Console.WriteLine($"tong {x} va {y}: " + tong);
+            Console.WriteLine($"hieu {x} va {y}: " + hieu);
+            Console.WriteLine($"tich {x} va {y}: " + tich);
+            Console.WriteLine($"thuong {x} va {y}: " + thuong);
+
+        }
+       
+
+    }
+}
+
